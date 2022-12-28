accc6f81809ab08fea9220714cff2f82bbb0d8d5
Zinaida <qktrey@gmail.com>
2022-12-28 22:06:29 +0300

    Add task 2, some changes for task 4

--- Homework/Homework 4/Tasks.py
+++ Homework/Homework 4/Tasks.py
@@ -11,10 +11,23 @@
 # print(p[:r])
 
 # 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
-#
-# n = int(input("Задайте натуральное число N: "))
-# for i in range(2,n+1):
 
+# number = int(input("Задайте натуральное число N: "))
+# list = []
+# len = number+1
+# for i in range(2, len):
+#     count = 0
+#     for n in range(2, i+1):
+#         if i % n == 0:
+#             count += 1
+#     if count == 1 and number % i == 0:
+#         list.append(i)
+#         number /= i
+#         while number % i == 0:
+#             list.append(i)
+#             number /= i
+#
+# print(list)
 
 # 3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной
 # последовательности.
@@ -33,30 +46,30 @@
 # и записать в файл многочлен степени k.
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
-import random
-n = int(input("Задайте натуральную степень k: "))
-x = ['*x**', '*x', ' = 0']
-with open("polynomial.txt", "w") as file:
-    file.write("")
-for i in range(n, 0, -1):
-    factor = random.randint(0, 100)
-    if factor != 0 and i != 1:
-        with open("polynomial.txt", "a") as file:
-            file.write(str(factor))
-            file.write(str(x[0]))
-            file.write(str(i))
-            file.write(" + ")
-    if factor != 0 and i == 1:
-        with open("polynomial.txt", "a") as file:
-            file.write(str(factor))
-            file.write(str(x[1]))
-            file.write(" + ")
-    if i == 0 and factor != 0:
-        with open("polynomial.txt", "a") as file:
-            file.write(str(factor))
-
-with open("polynomial.txt", "a") as file:
-    file.write(str(x[2]))
+# import random
+# n = int(input("Задайте натуральную степень k: "))
+# x = ['*x**', '*x', ' = 0']
+# with open("polynomial.txt", "w") as file:
+#     file.write("")
+# for i in range(n, -1, -1):
+#     factor = random.randint(0, 100)
+#     if factor != 0 and i != 1 and i != 0:
+#         with open("polynomial.txt", "a") as file:
+#             file.write(str(factor))
+#             file.write(str(x[0]))
+#             file.write(str(i))
+#             file.write(" + ")
+#     if factor != 0 and i == 1:
+#         with open("polynomial.txt", "a") as file:
+#             file.write(str(factor))
+#             file.write(str(x[1]))
+#             file.write(" + ")
+#     if i == 0 and factor != 0:
+#         with open("polynomial.txt", "a") as file:
+#             file.write(str(factor))
+#
+# with open("polynomial.txt", "a") as file:
+#     file.write(str(x[2]))
 
 
 # 5. Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
