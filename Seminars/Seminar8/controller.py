60cc25b7a66d39d1f63afc15d5f374e761e7b49a
Zinaida <qktrey@gmail.com>
2023-01-31 12:24:04 +0300

    Add Seminar8

--- Seminars/Seminar8/controller.py
+++ Seminars/Seminar8/controller.py
@@ -1,1 +1,41 @@
 import view
+
+main_book = {}
+names_list = []
+schoolworks_list = []
+def start():
+    while True:
+        number = view.menu()
+        if number == 1:                      #1: Добавить нового студента
+            name = view.get_student()
+            if name not in names_list:
+                names_list.append(name)
+                main_book[name] = {}
+                for less in schoolworks_list:
+                    main_book[name][less] = []
+            else:
+                view.repit_error()
+        elif number == 2:                     #2: Добавить предмет
+            schoolwork = view.get_schoolwork()
+            if schoolwork not in schoolworks_list:
+                schoolworks_list.append(schoolwork)
+                for name in names_list:
+                    main_book[name][schoolwork] = []
+            else:
+                view.repit_error()
+        elif number == 3:                   #3: Добавить оценку ученику по предмету
+            name = view.get_student()
+            if name not in names_list:
+                view.input_error()
+            schoolwork = view.get_schoolwork()
+            if schoolwork not in schoolworks_list:
+                view.input_error()
+            mark = view.get_mark()
+            main_book[name][schoolwork].append(mark)
+        elif number == 4:                # 4: Показать список учеников
+            print(main_book)
+        elif number == 5:                # 5: Показать лист оценок конкретного ученика
+            name = view.get_student()
+            print((main_book[name]))
+        elif number == 6:              #6:Выход
+            break
--- Seminars/Seminar8/view.py
+++ Seminars/Seminar8/view.py
@@ -1,6 +1,21 @@
+def input_error():
+    return print("Ошибка ввода.")
+def repit_error():
+    return print("Такая запись уже существует.")
+def menu():
+    start = int(input('Меню(введите цифру): \n 1: Добавить нового студента \n 2: Добавить предмет \n'
+                      ' 3: Добавить оценку ученику по предмету \n 4: Показать список учеников \n '
+                      '5: Показать лист оценок конкретного ученика \n 6:Выход \n'))
+
+    if start == 1 or start == 2 or start == 3 or start == 4 or start == 5 or start == 6:
+        return start
+    else:
+        return input_error()
 def get_student():
-    input('Введите имя')
-    input('Введите фамилию')
-def get_journal():
-    input('Введите предмет')
-    input('Введите оценку')
+    return input('Введите имя и фамилию: ')
+def get_schoolwork():
+    return input('Введите предмет: ')
+def get_mark():
+    return input('Введите оценку: ')
+
+
